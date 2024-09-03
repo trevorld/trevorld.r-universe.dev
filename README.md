33bf9a00050782102e07107a27f18b0618bb9c71
Trevor L. Davis <trevor.l.davis@gmail.com>
2024-09-03 15:56:05 -0700

    feat: Initial commit including 'packages.r'

---
+++ README.md
@@ -0,0 +1,9 @@
+This repository is to set up my own R-Universe universe:
+
+  * https://trevorld.r-universe.dev/builds
+  + https://docs.r-universe.dev/publish/set-up.html
+
+CRAN policies for suggested packages not on CRAN (e.g. the `Additional_repositories` `DESCRIPTION` field which can be a R-Universe universe):
+
+  * https://cran.r-project.org/doc/manuals/R-exts.html#Package-Dependencies-1
+  * https://cran.r-project.org/web/packages/policies.html#Source-packages-1
---
+++ packages.r
@@ -0,0 +1,40 @@
+library("jsonlite")
+
+urls_normal <- c(
+    "affiner",
+    "bittermelon",
+    "bracer",
+    "findpython",
+    "ggpattern",
+    "gridpattern",
+    "hexfont"
+)
+urls_r <- c(
+    "argparse",
+    "datetimeoffset",
+    # "getopt", 
+    "ledger",
+    "optparse",
+    "xmpdf"
+)
+
+urls_other <- list(
+    aRt = "https://github.com/nrennie/aRt",
+    r.oguelike = "https://github.com/matt-dray/r.oguelike",
+    truchet = "https://github.com/paezha/truchet"
+)
+
+df_normal <- data.frame(
+    package = urls_normal,
+    url = paste0("https://github.com/trevorld/", urls_normal)
+)
+df_r <- data.frame(
+    package = urls_r,
+    url = paste0("https://github.com/trevorld/r-", urls_r)
+)
+df_other <- data.frame(
+    package = names(urls_other),
+    url = unlist(urls_other, use.names = FALSE)
+)
+df <- rbind(df_normal, df_r, df_other)
+jsonlite::write_json(df, "packages.json", pretty = TRUE)

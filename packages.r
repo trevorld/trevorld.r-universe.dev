0bd76da2f2fee515656edb5141f2baf13376e14b
Trevor L. Davis <trevor.l.davis@gmail.com>
2026-01-27 23:17:39 -0800

    Add coolbutuseless's {svgparser}

--- packages.r
+++ packages.r
@@ -1,44 +1,45 @@
 library("jsonlite")
 
 urls_normal <- c(
-    "affiner",
-    "bittermelon",
-    "bracer",
-    "dee",
-    "findpython",
-    "ggpattern",
-    "gridpattern",
-    "hexfont",
-    "oblicubes",
-    "pnpmisc",
-    "sbgjackets"
+	"affiner",
+	"bittermelon",
+	"bracer",
+	"dee",
+	"findpython",
+	"ggpattern",
+	"gridpattern",
+	"hexfont",
+	"oblicubes",
+	"pnpmisc",
+	"sbgjackets"
 )
 urls_r <- c(
-    "argparse",
-    "datetimeoffset",
-    "getopt",
-    "ledger",
-    "optparse",
-    "xmpdf"
+	"argparse",
+	"datetimeoffset",
+	"getopt",
+	"ledger",
+	"optparse",
+	"xmpdf"
 )
 
 urls_other <- list(
-    aRt = "https://github.com/nrennie/aRt",
-    r.oguelike = "https://github.com/matt-dray/r.oguelike",
-    truchet = "https://github.com/paezha/truchet"
+	aRt = "https://github.com/nrennie/aRt",
+	r.oguelike = "https://github.com/matt-dray/r.oguelike",
+	svgparser = "https://github.com/coolbutuseless/svgparser",
+	truchet = "https://github.com/paezha/truchet"
 )
 
 df_normal <- data.frame(
-    package = urls_normal,
-    url = paste0("https://github.com/trevorld/", urls_normal)
+	package = urls_normal,
+	url = paste0("https://github.com/trevorld/", urls_normal)
 )
 df_r <- data.frame(
-    package = urls_r,
-    url = paste0("https://github.com/trevorld/r-", urls_r)
+	package = urls_r,
+	url = paste0("https://github.com/trevorld/r-", urls_r)
 )
 df_other <- data.frame(
-    package = names(urls_other),
-    url = unlist(urls_other, use.names = FALSE)
+	package = names(urls_other),
+	url = unlist(urls_other, use.names = FALSE)
 )
 df <- rbind(df_normal, df_r, df_other)
 jsonlite::write_json(df, "packages.json", pretty = TRUE)
